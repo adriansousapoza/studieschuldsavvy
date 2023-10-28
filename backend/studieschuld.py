a8ed6a2dbe1028b8615abad379578bc01941e16d
adriansousapoza <adrian.sousapoza@gmail.com>
2023-10-28 18:12:03 +0200

    created application which takes one constant as input and displays it

--- backend/studieschuld.py
+++ backend/studieschuld.py
@@ -13,42 +13,19 @@ import plotly.graph_objects as go
 import requests
 import json
 
-DEBT_BSC = 41899.78
-DEBT_MSC = 2406.86 + 11903.13
-DEBT_PRESTATIEBEURS = 439.20 + 1509.43
-MONTHLY_LOAN_MSC_2023_AUTUMN = 682.97 + 192.83
-MONTHLY_PRESTATIEBEURS_2023_AUTUMN = 120.96 + 439.20
-MONTHLY_LOAN_MSC_2024_SPRING = 751.27 + 192.83
-MONTHLY_PRESTATIEBEURS_2024_SPRING = 466.69 + 120.96
-MONTHLY_LOAN_MSC_2024_AUTUMN = 751.27 + 210.83
-TODAY = datetime.date.today()
-END_DATE_ZERO_PERCENT = datetime.date(2026, 12, 31)
-END_DATE_STUDY = datetime.date(2024, 10, 31)
-END_DATE_LOAN_MSC = datetime.date(2023, 12, 31)
-
-INTEREST_RATES = [
-    [2024, 2.56],
-    [2023, 0.46],
-    [2022, 0.00],
-    [2021, 0.00],
-    [2020, 0.00],
-    [2019, 0.00],
-    [2018, 0.00],
-    [2017, 0.00],
-    [2016, 0.01],
-    [2015, 0.12],
-    [2014, 0.81],
-    [2013, 0.60],
-    [2012, 1.39],
-    [2011, 1.50],
-    [2010, 2.39],
-    [2009, 3.58],
-    [2008, 4.17],
-    [2007, 3.70],
-    [2006, 2.74]
-]
-
-def total_debt_func(init, final, interest_bool=True):
+def total_debt_func(init, final, 
+                    DEBT_BSC, 
+                    DEBT_MSC, 
+                    DEBT_PRESTATIEBEURS, MONTHLY_LOAN_MSC_2023_AUTUMN, 
+                    MONTHLY_PRESTATIEBEURS_2023_AUTUMN,
+                    MONTHLY_LOAN_MSC_2024_SPRING,
+                    MONTHLY_PRESTATIEBEURS_2024_SPRING,
+                    MONTHLY_LOAN_MSC_2024_AUTUMN,
+                    END_DATE_STUDY, 
+                    END_DATE_LOAN_MSC, 
+                    END_DATE_ZERO_PERCENT,
+                    INTEREST_RATES,
+                    interest_bool=True):
     T = np.arange(init, final, 1/12)
     
     total_debt = np.zeros(len(T))
@@ -57,8 +34,8 @@ def total_debt_func(init, final, interest_bool=True):
     debt_master = np.zeros(len(T))
 
     debt_bachelor[0] = DEBT_BSC
-    debt_prestatiebeurs[0] = DEBT_PRESTATIEBEURS
-    debt_master[0] = DEBT_MSC
+    debt_prestatiebeurs[0] = DEBT_MSC
+    debt_master[0] = DEBT_PRESTATIEBEURS
     total_debt[0] = debt_bachelor[0] + debt_prestatiebeurs[0] + debt_master[0]
     
     time = np.zeros(len(T))
@@ -201,21 +178,87 @@ def plot_inflation_rates(periods, inflation_rates):
     fig = go.Figure(data=data, layout=layout)
     return fig
 
+def get_inflation_rates():
+    base_url = 'https://opendata.cbs.nl/ODataApi/OData/70936ned/'
+    collection = 'TypedDataSet'
+    data = None
+
+    collection_url = f"{base_url}{collection}"
+    response = requests.get(collection_url)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+    else:
+        print(f"Failed to retrieve data from {collection}. Status code: {response.status_code}")
+
+    if data is not None:
+        inflation_info = []
+        for record in data['value']:
+            period_str = record['Perioden']
+            year, month = int(period_str[:4]), int(period_str[6:8])
+            if year >= 2006 and month != 0:
+                period_date = datetime.datetime(year, month, 1)
+                formatted_period = period_date.strftime('%m/%Y')
+
+                inflation_info.append({
+                    'period': formatted_period,
+                    'inflation_rate': record['JaarmutatieCPI_1']
+                })
+
+        periods = [info['period'] for info in inflation_info]
+        inflation_rates = [info['inflation_rate'] for info in inflation_info]
+
+        return periods, inflation_rates
+
+        
+    else:
+        print("Data retrieval failed. 'data' is not set.")
+        return None, None
+    
+DEBT_BSC = 41899.78
+DEBT_MSC = 2406.86 + 11903.13
+DEBT_PRESTATIEBEURS = 439.20 + 1509.43
+MONTHLY_LOAN_MSC_2023_AUTUMN = 682.97 + 192.83
+MONTHLY_PRESTATIEBEURS_2023_AUTUMN = 120.96 + 439.20
+MONTHLY_LOAN_MSC_2024_SPRING = 751.27 + 192.83
+MONTHLY_PRESTATIEBEURS_2024_SPRING = 466.69 + 120.96
+MONTHLY_LOAN_MSC_2024_AUTUMN = 751.27 + 210.83
+TODAY = datetime.date.today()
+END_DATE_ZERO_PERCENT = datetime.date(2026, 12, 31)
+END_DATE_STUDY = datetime.date(2024, 10, 31)
+END_DATE_LOAN_MSC = datetime.date(2023, 12, 31)
+
+INTEREST_RATES = [
+    [2024, 2.56],
+    [2023, 0.46],
+    [2022, 0.00],
+    [2021, 0.00],
+    [2020, 0.00],
+    [2019, 0.00],
+    [2018, 0.00],
+    [2017, 0.00],
+    [2016, 0.01],
+    [2015, 0.12],
+    [2014, 0.81],
+    [2013, 0.60],
+    [2012, 1.39],
+    [2011, 1.50],
+    [2010, 2.39],
+    [2009, 3.58],
+    [2008, 4.17],
+    [2007, 3.70],
+    [2006, 2.74]
+]
+    
 init = TODAY.year + TODAY.month/12
 final = TODAY.year + TODAY.month/12 + 10
-time, total_debt, debt_bachelor, debt_prestatiebeurs, debt_master = total_debt_func(init, final)
-time2, total_debt2, _, _, _ = total_debt_func(init, final, interest_bool=False)
+time, total_debt, debt_bachelor, debt_prestatiebeurs, debt_master = total_debt_func(init, final, DEBT_BSC=DEBT_BSC, DEBT_MSC=DEBT_MSC, DEBT_PRESTATIEBEURS=DEBT_PRESTATIEBEURS, MONTHLY_LOAN_MSC_2023_AUTUMN=MONTHLY_LOAN_MSC_2023_AUTUMN, MONTHLY_PRESTATIEBEURS_2023_AUTUMN=MONTHLY_PRESTATIEBEURS_2023_AUTUMN, MONTHLY_LOAN_MSC_2024_SPRING=MONTHLY_LOAN_MSC_2024_SPRING, MONTHLY_PRESTATIEBEURS_2024_SPRING=MONTHLY_PRESTATIEBEURS_2024_SPRING, MONTHLY_LOAN_MSC_2024_AUTUMN=MONTHLY_LOAN_MSC_2024_AUTUMN, END_DATE_STUDY=END_DATE_STUDY, END_DATE_LOAN_MSC=END_DATE_LOAN_MSC, END_DATE_ZERO_PERCENT=END_DATE_ZERO_PERCENT, INTEREST_RATES=INTEREST_RATES)
+time2, total_debt2, _, _, _ = total_debt_func(init, final, DEBT_BSC=DEBT_BSC, DEBT_MSC=DEBT_MSC, DEBT_PRESTATIEBEURS=DEBT_PRESTATIEBEURS, MONTHLY_LOAN_MSC_2023_AUTUMN=MONTHLY_LOAN_MSC_2023_AUTUMN, MONTHLY_PRESTATIEBEURS_2023_AUTUMN=MONTHLY_PRESTATIEBEURS_2023_AUTUMN, MONTHLY_LOAN_MSC_2024_SPRING=MONTHLY_LOAN_MSC_2024_SPRING, MONTHLY_PRESTATIEBEURS_2024_SPRING=MONTHLY_PRESTATIEBEURS_2024_SPRING, MONTHLY_LOAN_MSC_2024_AUTUMN=MONTHLY_LOAN_MSC_2024_AUTUMN, END_DATE_STUDY=END_DATE_STUDY, END_DATE_LOAN_MSC=END_DATE_LOAN_MSC, END_DATE_ZERO_PERCENT=END_DATE_ZERO_PERCENT, INTEREST_RATES=INTEREST_RATES, interest_bool=False)
 x_end = END_DATE_STUDY.year + END_DATE_STUDY.month/12
-
 
 debt_fig = plot_total_debt(time, total_debt, total_debt2, x_end)
 debt_individual_fig = plot_individual_debts(time, debt_bachelor, debt_prestatiebeurs, debt_master, x_end)
 debt_difference_fig = plot_debt_difference(time, total_debt, total_debt2, x_end)
-
-#debt_fig.show()
-#debt_individual_fig.show()
-#debt_difference_fig.show()
-
 
 df = pd.DataFrame({'time':np.round(time,decimals=2),
                    'debt withou interest':np.round(total_debt2,decimals=2),
@@ -224,43 +267,10 @@ df = pd.DataFrame({'time':np.round(time,decimals=2),
 
 #df.to_csv(r'/home/asp/Downloads/debt.csv', sep=',')
 
-
-base_url = 'https://opendata.cbs.nl/ODataApi/OData/70936ned/'
-collection = 'TypedDataSet'
-data = None
-
-collection_url = f"{base_url}{collection}"
-response = requests.get(collection_url)
-
-if response.status_code == 200:
-    data = json.loads(response.text)
-    print(f"Data from {collection}:")
-    print(json.dumps(data, indent=2))
-else:
-    print(f"Failed to retrieve data from {collection}. Status code: {response.status_code}")
-
-if data is not None:
-    inflation_info = []
-    for record in data['value']:
-        period_str = record['Perioden']
-        year, month = int(period_str[:4]), int(period_str[6:8])
-        print(f"Year: {year}, Month: {month}")
-        if year >= 2006 and month != 0:
-            period_date = datetime.datetime(year, month, 1)
-            formatted_period = period_date.strftime('%m/%Y')
-
-            inflation_info.append({
-                'period': formatted_period,
-                'inflation_rate': record['JaarmutatieCPI_1']
-            })
-
-    periods = [info['period'] for info in inflation_info]
-    inflation_rates = [info['inflation_rate'] for info in inflation_info]
-
-    
-else:
-    print("Data retrieval failed. 'data' is not set.")
-
-
+periods, inflation_rates = get_inflation_rates()
 inflation_plot = plot_inflation_rates(periods, inflation_rates)
-inflation_plot.show()
+
+#debt_fig.show()
+#debt_individual_fig.show()
+#debt_difference_fig.show()
+#inflation_plot.show()
---
+++ frontend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).parents[1].absolute()
+sys.path.append(str(parent_dir))
+
+from backend.studieschuld import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def form():
+    return render_template('input_form.html')
+
+@app.route('/display', methods=['POST'])
+def display():
+    debt_bsc = request.form['debt_bsc']
+
+    return render_template('display.html', debt_bsc=debt_bsc) 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
